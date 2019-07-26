a1b106cefec04ffaa36fe4481ee86dedd1b089cc
yanuz93 <yanuzpurnomo@gmail.com>
2019-07-26 10:00:04 +0700

    merge

--- cloning/urls.py
+++ cloning/urls.py
@@ -4,6 +4,12 @@ from . import views
 urlpatterns = [
     path('',views.home, name='home'),
     path('detail/',views.detail, name='halaman_detail'),
-    path('search/',views.search, name='halaman_search'),
+    path('search/page<int:page>/',views.search, name='halaman_search'),
     path('kota/',views.kota, name='cari_kota'),
+    path('filter/',views.filter, name='filter_by_place'),
+    path('filter/',views., name='filter_by_category'),
+    path('tambah/',views.input_resto, name'tambah_foto'),
+    path('tambah/',views.input_resto, name'tambah_foto'),
+    path('tambah/',views.input_resto, name'tambah_foto'),
+
 ]
